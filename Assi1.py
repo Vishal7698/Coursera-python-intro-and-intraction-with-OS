d1c60b06f9d731c23ecfec7c091950d9c31925e4
Vishal Hase <Vishal7698@github.com>
2020-05-25 15:41:20 +0530

    first question

---
+++ Assi1.py
@@ -0,0 +1,7 @@
+# assignment 1
+# 1.Write a program which contains one function named as Fun(). That function should display
+# “Hello from Fun” on console
+
+def fun():
+    print("hello from fun")
+fun()
