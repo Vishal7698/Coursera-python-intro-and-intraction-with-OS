6fbbf2810fb1c7244b13eeea6111517714df73fe
vishal hase <vishalhase1111@gmail.com>
2020-07-11 17:33:23 +0530

    points and google crash course project

---
+++ number system Coversion.py
@@ -0,0 +1,8 @@
+def main():
+    no=int(input("enter the number"))
+    print("decimal format is {}".format(no))
+    print("binary format is"+bin(no))
+    print("hexadecimal format"+hex(no))
+
+if __name__ == '__main__':
+    main()
